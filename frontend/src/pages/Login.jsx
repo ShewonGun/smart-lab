373169c84a06e6390193fb69abef080cfc9db77a
Shewon Gunarathne <shewongunarathne@gmail.com>
2025-03-13 08:06:29 +0530

    commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState('Sign Up')
+
+  const [email, setEmail] = useState([])
+  const [password, setPassword] = useState([])
+  const [name, setName] = useState([])
+
+  const onSubmitHandler = async (event) => {
+      event.preventDefault()
+  }
+
+
   return (
-    <div>
+    <form className=''>
         
-    </div>
+    </form>
   )
 }
 
